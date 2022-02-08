7afdad360a5717c10b89f60ab39ab3f2e6c12890
Zack Murphy <zackm@rkdfamily.com>
2022-02-08 10:23:00 -0500

    - Added (ugly) files for web application - Included CSS and JS file for scripts, will be trimmed as needed

--- web/main.js
+++
@@ -1,9 +0,0 @@
-import { createBoard } from "./connect4.js";
-
-window.addEventListener("DOMContentLoaded", () => {
-  // Initialize the UI.
-  const board = document.querySelector(".app");
-  const websocket = new WebSocket("ws://localhost:8888/");
-  createBoard(board);
-  
-});
