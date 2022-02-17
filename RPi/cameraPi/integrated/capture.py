fa7a305954b605ddd24bd45d453dc4f3db14c792
Dongheon97 <jdd04063@gmail.com>
2022-02-17 15:13:40 +0000

    backup CameraPi

---
+++ RPi/cameraPi/integrated/capture.py
@@ -0,0 +1,20 @@
+import cv2
+
+class Picture(object):
+    def __init__(self, source):
+        self._source = source
+
+    def takePic(self, path):
+        # /dev/video0
+        cap = cv2.VideoCapture(0)
+        ret, frame = cap.read()
+
+        #cv2.imshow("frame", frame)
+        #cv2.waitKey(0)
+        # Setting file path & format 
+        outputFile = path + ".jpeg"
+        # Save image
+        cv2.imwrite(outputFile, frame)
+
+
+
