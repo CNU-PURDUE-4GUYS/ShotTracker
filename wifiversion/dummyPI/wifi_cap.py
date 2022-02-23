7098166ba7ce087ccea213be974125f4d333f301
Jisoo Choi <jisoochoi7561@gmail.com>
2022-02-23 16:34:09 -0500

    final

--- wifiversion/dummyPI/wifi_cap.py
+++ wifiversion/dummyPI/wifi_cap.py
@@ -1,3 +1,4 @@
+from capture import Picture
 import os
 import base64
 import json
@@ -6,16 +7,16 @@ import asyncio
 import websockets
 
 from utils import *
-# python file for "dummy raspberry pi." It sends fixed "refer.jpg" and "image.jpg". REAL raspberry pi file is different from this one.
+
 async def pihandler():
-    # Sets WebSockets URI and other variables
-    uri = "ws://192.168.2.116:8888"
+    # real pi
+    uri = "ws://172.20.10.7:8888"
     set_id = ""
     user_id = "jisoo"
     pi_id = "jisoopi1"
-    # Connects to Websocket server
+    # websocket connection
     async with websockets.connect(uri) as websocket:
-        # send Hello to Websocket server.
+        # signaling
         hello = json.dumps({
                 "command":"init",
                 "user_id":user_id,
@@ -26,18 +27,20 @@ async def pihandler():
         async for message in websocket:
                 event = json.loads(message)
                 command = event["command"]
-                # when receives newset, set "set_id"
                 if command == "newSet":
                         set_id = event["set_id"]
                 elif command == "takeRef":
-                # when receivs "takeRef", take ref and send result.
-                        # take reference
+                        pic = Picture(None)
+                        path = "/home/pi/refer"
+                        pic.takePic(path)	                       # take reference
                         # then upload
                         result = refer_json(user_id,pi_id,set_id)
                         print(len(result))
                         await websocket.send(result)
                 elif command == "takePhoto":
-                        # when receivs "takePhoto", take photo and send result.
+                        pic = Picture(None)
+                        path = "/home/pi/image"
+                        pic.takePic(path)
                         # take photo
                         # then upload
                         await websocket.send(image_json(user_id,pi_id,set_id))
@@ -47,5 +50,6 @@ async def pihandler():
 
 if __name__ == "__main__":
     asyncio.run(pihandler())
+
 
 
